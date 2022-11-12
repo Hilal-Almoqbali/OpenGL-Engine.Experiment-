f4f3968ae03bf4897f1b95f365744063801d4202
Hilal <72453298+Hilal-Almoqbali@users.noreply.github.com>
2022-11-12 12:54:05 +0000

    do stuff

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-g++ ./src/*.cc ./src/*.c -I include -I Engine -lglfw
+echo 'build main: 1'
+echo 'build test: 2'
+read input
+if test $input -eq 1
+then
+    g++ ./src/*.cc ./src/*.c -I include -I Engine -lglfw
+elif test $input -eq 2
+then
+    g++ ./test/*.cc ./src/*.c -I include -I Engine -I test -lglfw
+fi
