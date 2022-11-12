ffd6bd6383d9eeee4026ca59d420d4a28b778092
Hilal <72453298+Hilal-Almoqbali@users.noreply.github.com>
2022-11-12 08:09:51 +0000

    add glm

--- Engine/Renderer.h
+++ Engine/Renderer.h
@@ -1,6 +1,9 @@
 #pragma once
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
 #include<iostream>
 class Engine
 {
